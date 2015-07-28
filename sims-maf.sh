3f39625d8b297851c5e20022492d72a4d5334be5
marcusbooyah <mbowyer@lsst-dev.ncsa.illinois.edu>
2015-07-28 17:21:35 -0500

    testing travis

--- sims-maf.sh
+++ sims-maf.sh
@@ -3,8 +3,8 @@ echo "Setting up enviromnent."
 source eups-setups.sh
 #conda install lsst-sims-maf -y
 pip install runipy
-eups declare -m none -r none sims_sed_library 2014.10.06
-eups declare -m none -r none sims_dustmaps 0.10.1
+#eups declare -m none -r none sims_sed_library 2014.10.06
+#eups declare -m none -r none sims_dustmaps 0.10.1
 setup sims_maf
 git pull > /dev/null 2>&1
 cd tutorials
@@ -15,21 +15,34 @@ if [[ ! -f enigma_1189_sqlite.db ]] 2>"f.out"; then
 	wget -q -O - http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz | gunzip -c > enigma_1189_sqlite.db
 fi 
 ERROR=0
-git diff --name-only $TRAVIS_BRANCH HEAD
-for f in *.ipynb; do
-	echo "Processing $f"
-	if [[ "$f" == SDSSSlicer.ipynb ]]; then
-		continue
-	fi
-	if [[ "$f" == MAFCameraGeom.ipynb ]]; then
-          continue
-       	fi
-	if runipy "$f" "tested-$f" 2>"$f.out"; then
-		echo "$f" passed.
-	else
-		echo "$f" failed.
-		ERROR=1
-	fi
-done
+if [[ $TRAVIS_PULL_REQUEST != "false" ]]; then
+	cd ../
+	git diff --name-only $TRAVIS_BRANCH HEAD > changes.out
+	for f in changes.out; do 
+		echo "Processing $f"
+		if runipy "$f" "tested-$f" 2>"$f.out"; then
+                	echo "$f" passed.
+        	else
+            		echo "$f" failed.
+                	ERROR=1
+        	fi
+	done
+else
+	for f in *.ipynb; do
+		echo "Processing $f"
+		if [[ "$f" == SDSSSlicer.ipynb ]]; then
+			continue
+		fi
+		if [[ "$f" == MAFCameraGeom.ipynb ]]; then
+          	continue
+       		fi
+		if runipy "$f" "tested-$f" 2>"$f.out"; then
+			echo "$f" passed.
+		else
+			echo "$f" failed.
+			ERROR=1
+		fi
+	done
+fi
 exit $ERROR
 
