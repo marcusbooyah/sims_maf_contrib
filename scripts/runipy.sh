43d7bcf7e16309f291039ce0c6ac40d6209fc731
marcusbooyah <booyah@uw.edu>
2015-08-23 20:14:31 -0700

    new branch

--- scripts/runipy.sh
+++ scripts/runipy.sh
@@ -1,15 +1,22 @@
 set -e
 ERROR=0
+mkdir fails
 while read line
         do
           	echo "Processing $line"
                 if runipy "./$line" "./$line-tested.ipynb" 2>"./$line.out"; then
-                                echo "$line" passed.
+                        echo "$line" passed.
                         echo
                 else
-                                echo "$line" failed.
-                        ERROR=1
+                        echo "$line" failed.
+                        mv "./$line-tested.ipynb" "./$line-failed.ipynb"
+			ERROR=1
                         echo
                 fi
         done < notebooks.out
-exit $ERROR
+if [ $ERROR = 1 ]; then
+	echo "The following notebooks failed"
+	find . -name "*failed.ipynb" | xargs tar cvf - | (cd ./fails ; tar xfp -)
+else
+	echo "All notebooks passed"
+fi
